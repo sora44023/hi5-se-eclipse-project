d1e75d11fe64677ad0325e2887c7fa1c3993ad12
sora44023 <iwataka369167.kuma@gmail.com>
2022-06-02 12:31:32 +0900

    Update kadai2-3

--- src/kadai2_3/HelloThread.java
+++ src/kadai2_3/HelloThread.java
@@ -1,5 +1,40 @@
 package kadai2_3;
 
-public class HelloThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class HelloThread implements Runnable{
+	int count;
+	Socket sock;
+
+	public HelloThread(Socket sock, int count) {
+		this.sock = sock;
+		this.count = count;
+	}
+
+	public void run() {
+		try {
+			System.out.println("Connected: Client No. " + count + " (" + sock.getInetAddress().getHostName() + ").");
+			BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream())); // 入力
+			PrintWriter out = new PrintWriter(sock.getOutputStream(), true); // 出力
+
+			out.println();
+			out.println("Hi, you are Client No. " + count + ".");
+			while (true) {
+				out.println("What's your name?");
+				String name = in.readLine(); // 一行入力を受け取る
+				if (name.equals("bye"))
+					break;
+				out.println("Hello, " + name + "!");
+			}
+			in.close();
+			out.close();
+			sock.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/kadai2_3/MyHelloServer.java
+++ src/kadai2_3/MyHelloServer.java
@@ -1,9 +1,6 @@
 package kadai2_3;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -23,23 +20,7 @@ public class MyHelloServer {
 
 				// 接続されたときの処理
 				count++;
-				System.out.println("Connected: Client No. " + count + " ("
-						+ sock.getInetAddress().getHostName() + ").");
-				BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream())); // 入力
-				PrintWriter out = new PrintWriter(sock.getOutputStream(), true); // 出力
-
-				out.println();
-				out.println("Hi, you are Client No. " + count + ".");
-				while (true) {
-					out.println("What's your name?");
-					String name = in.readLine(); // 一行入力を受け取る
-					if (name.equals("bye"))
-						break;
-					out.println("Hello, " + name + "!");
-				}
-				in.close();
-				out.close();
-				sock.close();
+				new Thread(new HelloThread(sock, count));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
