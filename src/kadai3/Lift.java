95f847ea97cf4e2a95f78a2f2907f624e3fbaa92
sora44023 <iwataka369167.kuma@gmail.com>
2022-06-16 11:52:59 +0900

    6/16授業終了したとこまで

--- src/kadai3/Lift.java
+++ src/kadai3/Lift.java
@@ -1,21 +1,21 @@
 package kadai3;
 
 public class Lift implements Runnable {
-	private int psgnum;	// 乗せている乗客番号．乗せていなければ負の値
-	private int delay;	// 遅さ（1動作の間隔）
-	
+	private int psgnum; // 乗せている乗客番号．乗せていなければ負の値
+	private int delay; // 遅さ（1動作の間隔）
+
 	private static int seqNo = 1; // 通し番号
 	private int no = 0; // リフト番号
-	
+
 	final static int SHIFT_WIDTH = 32; // 出力の際の空白の幅
-	
+
 	public Lift(int delay) {
-		this.delay = delay;		
+		this.delay = delay;
 		psgnum = -1;
 		no = seqNo;
 		seqNo++;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
@@ -23,13 +23,20 @@ public class Lift implements Runnable {
 				int id = PassengerCounter.getNextPassengerId();
 				if (id < 0)
 					break;
-				for(int i=0; i<4; i++) {
-					Main.show(no,id,i);
+				Main.show("Lift" + no + ": Welcome Passenger No." + id, (no - 1) * SHIFT_WIDTH);
+				String s = "[" + String.format("%2d", id) + "]";
+				for (int i = 0; i < 5; i++) {
+					Main.show("Lift" + no + ": " + s, (no - 1) * SHIFT_WIDTH);
+					s = "----|" + s;
+				}
+				Main.show("Passenger " + id + ": Thanks!", (no - 1) * SHIFT_WIDTH);
+				for (int i = 0; i < 5; i++) {
+					Main.show("Lift" + no + ": " + s, (no - 1) * SHIFT_WIDTH);
 				}
 				Thread.sleep(delay * 100);
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}	
+	}
 }
--- src/kadai3/Main.java
+++ src/kadai3/Main.java
@@ -1,9 +1,6 @@
 package kadai3;
 
 public class Main {
-	
-	final static int SHIFT_WIDTH = 32; // 出力の際の空白の幅
-	
 	public static void main(String[] args) {
 		Lift lift1 = new Lift(2);
 		Thread t1 = new Thread(lift1);
@@ -12,23 +9,10 @@ public class Main {
 		Thread t2 = new Thread(lift2);
 		t2.start();		
 	}
-//	public synchronized static void show(String s, int shift) {
-//		// shift分だけ空白をあけてsを出力する
-//		for (int i=0; i<shift; i++)
-//			System.out.print(' ');
-//		System.out.println(s);
-//	}
-	public synchronized static void show(int no, int id, int currentFloor) {
+	public synchronized static void show(String s, int shift) {
 		// shift分だけ空白をあけてsを出力する
-		for (int i=0; i<(no - 1) * SHIFT_WIDTH; i++)
+		for (int i=0; i<shift; i++)
 			System.out.print(' ');
-		if(currentFloor==3) {
-			System.out.println("Passenger " + id + ": Thanks!");
-		}else {
-			for(int i=0; i<currentFloor; i++) {
-				System.out.print("----|");
-			}
-			System.out.printf("[%2d]\n",id);
-		}
+		System.out.println(s);
 	}
 }
