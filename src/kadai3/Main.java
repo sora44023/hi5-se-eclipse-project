96dcba8c7a36d1f548fd96d43b2d00ca5a5142fe
sora44023 <iwataka369167.kuma@gmail.com>
2022-06-23 11:27:25 +0900

    値の調整

--- src/kadai3/Main.java
+++ src/kadai3/Main.java
@@ -8,9 +8,9 @@ public class Main {
 		Lift lift2 = new Lift(3);
 		Thread t2 = new Thread(lift2);
 		t2.start();
-		Lift lift3 = new Lift(4);
-		Thread t3 = new Thread(lift3);
-		t3.start();	
+//		Lift lift3 = new Lift(4);
+//		Thread t3 = new Thread(lift3);
+//		t3.start();	
 	}
 	public synchronized static void show(String s, int shift) {
 		// shift分だけ空白をあけてsを出力する
