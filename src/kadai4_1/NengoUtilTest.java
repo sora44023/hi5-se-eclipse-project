dfe7847dce49750ac1c2190fa74541077030f8c5
sora44023 <iwataka369167.kuma@gmail.com>
2022-06-30 11:45:17 +0900

    format code

--- src/kadai4_1/NengoUtilTest.java
+++ src/kadai4_1/NengoUtilTest.java
@@ -16,32 +16,32 @@ public class NengoUtilTest {
 	public void isHeiseiYearに1970を与えるとfalseを返す() {
 		assertThat(nu.isHeiseiYear(1970), is(false));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに1988を与えるとfalseを返す() {
 		assertThat(nu.isHeiseiYear(1988), is(false));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに1989を与えるとfalseを返す() {
 		assertThat(nu.isHeiseiYear(1989), is(true));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに2000を与えるとtrueを返す() {
 		assertThat(nu.isHeiseiYear(2000), is(true));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに2019を与えるとtrueを返す() {
 		assertThat(nu.isHeiseiYear(2019), is(true));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに2020を与えるとfalseを返す() {
 		assertThat(nu.isHeiseiYear(2020), is(false));
 	}
-	
+
 	@Test
 	public void isHeiseiYearに2030を与えるとfalseを返す() {
 		assertThat(nu.isHeiseiYear(2030), is(false));
