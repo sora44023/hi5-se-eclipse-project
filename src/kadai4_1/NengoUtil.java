c4fb5335fa0a189ec2f8a79452ea1608fe970b25
sora44023 <iwataka369167.kuma@gmail.com>
2022-06-30 11:18:30 +0900

    課題4-1できた

---
+++ src/kadai4_1/NengoUtil.java
@@ -0,0 +1,10 @@
+package kadai4_1;
+
+public class NengoUtil {
+	boolean isHeiseiYear(int year) {
+		if (1989 <= year && year <= 2019)
+			return true;
+		else
+			return false;
+	}
+}
---
+++ src/kadai4_1/NengoUtilTest.java
@@ -0,0 +1,49 @@
+package kadai4_1;
+
+import org.junit.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class NengoUtilTest {
+	private NengoUtil nu;
+
+	@Before // 各テストの実行前に行われる共通処理
+	public void setUp() throws Exception {
+		nu = new NengoUtil();
+	}
+
+	@Test
+	public void isHeiseiYearに1970を与えるとfalseを返す() {
+		assertThat(nu.isHeiseiYear(1970), is(false));
+	}
+	
+	@Test
+	public void isHeiseiYearに1988を与えるとfalseを返す() {
+		assertThat(nu.isHeiseiYear(1988), is(false));
+	}
+	
+	@Test
+	public void isHeiseiYearに1989を与えるとfalseを返す() {
+		assertThat(nu.isHeiseiYear(1989), is(true));
+	}
+	
+	@Test
+	public void isHeiseiYearに2000を与えるとtrueを返す() {
+		assertThat(nu.isHeiseiYear(2000), is(true));
+	}
+	
+	@Test
+	public void isHeiseiYearに2019を与えるとtrueを返す() {
+		assertThat(nu.isHeiseiYear(2019), is(true));
+	}
+	
+	@Test
+	public void isHeiseiYearに2020を与えるとfalseを返す() {
+		assertThat(nu.isHeiseiYear(2020), is(false));
+	}
+	
+	@Test
+	public void isHeiseiYearに2030を与えるとfalseを返す() {
+		assertThat(nu.isHeiseiYear(2030), is(false));
+	}
+}
