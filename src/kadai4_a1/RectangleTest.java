297b9c0cd8249274a79f5eb4746c0cfb3032ef2a
sora44023 <iwataka369167.kuma@gmail.com>
2022-07-20 18:20:39 +0900

    修正

--- src/kadai4_a1/RectangleTest.java
+++ src/kadai4_a1/RectangleTest.java
@@ -16,47 +16,47 @@ public class RectangleTest {
 	}
 	
 	@Test
-	public void checkOverlapにrがthisより小さいときINを返す() {
+	public void rがthisより小さいときINを返す() {
 		assertThat(r.checkOverlap(new Rectangle(5,5,2,2)), is(OverlapState.IN));
 	}
 	
 	@Test
-	public void checkOverlapにrとthisが同じ大きさであるときOVERLAPを返す() {
+	public void rとthisが同じ大きさであるときOVERLAPを返す() {
 		assertThat(r.checkOverlap(new Rectangle(4,4,4,4)), is(OverlapState.OVERLAP));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの外側にあるときOUTを返す() {
+	public void rがthisの外側にあるときOUTを返す() {
 		assertThat(r.checkOverlap(new Rectangle(5,1,2,2)), is(OverlapState.OUT));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの右の辺に重なるとOVERLAPを返す() {
+	public void rがthisの右の辺に重なるとOVERLAPを返す() {
 		assertThat(r.checkOverlap(new Rectangle(5,5,3,3)), is(OverlapState.OVERLAP));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの左の辺に重なるOVERLAPを返す() {
+	public void rがthisの左の辺に重なるOVERLAPを返す() {
 		assertThat(r.checkOverlap(new Rectangle(3,3,2,2)), is(OverlapState.OVERLAP));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの上の辺に重なるOVERLAPを返す() {
+	public void rがthisの上の辺に重なるOVERLAPを返す() {
 		assertThat(r.checkOverlap(new Rectangle(5,3,2,2)), is(OverlapState.OVERLAP));
 	}
 
 	@Test
-	public void checkOverlapにrがthisの下の辺に重なるOVERLAPを返す() {
+	public void rがthisの下の辺に重なるOVERLAPを返す() {
 		assertThat(r.checkOverlap(new Rectangle(5,7,2,2)), is(OverlapState.OVERLAP));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの左外側にあるときOUTを返す() {
+	public void rがthisの左外側にあるときOUTを返す() {
 		assertThat(r.checkOverlap(new Rectangle(1,1,10,2)), is(OverlapState.OUT));
 	}
 	
 	@Test
-	public void checkOverlapにrがthisの右外側にあるときOUTを返す() {
+	public void rがthisの右外側にあるときOUTを返す() {
 		assertThat(r.checkOverlap(new Rectangle(9,2,2,10)), is(OverlapState.OUT));
 	}
 	
