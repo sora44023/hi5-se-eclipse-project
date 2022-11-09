558ed5df9ceabb31ea97b536376cba359480f6d9
sora44023 <iwataka369167.kuma@gmail.com>
2022-11-09 11:13:34 +0900

    必要なフィールドを追加

--- src/kadai6/Game.java
+++ src/kadai6/Game.java
@@ -1,15 +1,22 @@
 package kadai6;
 
 public class Game {
+	private int stoneCount = 12;
 	private Player[] players;
+	private static int gameCount = 0;
 	
 	public Game(Player p1, Player p2) {
 		players = new Player[2];
 		players[0] = p1;
 		players[1] = p2;
+		gameCount = getGameCount() + 1;
 	}
 	
 	void start() {
 		
+	}
+
+	public static int getGameCount() {
+		return gameCount;
 	}
 }
