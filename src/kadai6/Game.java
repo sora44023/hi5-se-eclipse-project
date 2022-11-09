3842f5f6aab5cd6802cdb809df79aa4a16bb2768
sora44023 <iwataka369167.kuma@gmail.com>
2022-11-09 11:54:19 +0900

    11/9授業終了

--- src/kadai6/Game.java
+++ src/kadai6/Game.java
@@ -4,16 +4,29 @@ public class Game {
 	private int stoneCount = 12;
 	private Player[] players;
 	private static int gameCount = 0;
-	
+
 	public Game(Player p1, Player p2) {
 		players = new Player[2];
 		players[0] = p1;
 		players[1] = p2;
 		gameCount = getGameCount() + 1;
 	}
+
+	void printStone() {
+		System.out.print("stone: ");
+		for(int i=0; i<stoneCount; i++)
+			System.out.print("@");
+		System.out.println("[" + stoneCount + "]");
+	}
 	
 	void start() {
-		
+		System.out.println("[Game No." + getGameCount() + "]");
+		do {
+			for (int i = 0; i < 1; i++) {
+				stoneCount = players[i].takeStones(stoneCount);
+				System.out.println(players[i].toString() + "took");
+			}
+		} while (stoneCount < 1);
 	}
 
 	public static int getGameCount() {
