ea8b68689a78db16c23bc8c893feeff2c35a6678
sora44023 <iwataka369167.kuma@gmail.com>
2022-11-24 11:36:19 +0900

    多分いい感じ

--- src/kadai6/ComputerPlayer.java
+++ src/kadai6/ComputerPlayer.java
@@ -3,17 +3,19 @@ package kadai6;
 import java.util.Random;
 
 class ComputerPlayer extends Player {
-	
+
 	Random rnd = new Random();
-	
+
 	ComputerPlayer(String name) {
 		super(name);
 	}
 
 	@Override
 	int takeStones(int stoneCount) {
-		if(stoneCount < 3)
-			return stoneCount - (rnd.nextInt(1) + 1);
+		if (stoneCount < 1)
+			return -1;
+		else if (stoneCount < 3)
+			return stoneCount - (rnd.nextInt(stoneCount) + 1);
 		else
 			return stoneCount - (rnd.nextInt(3) + 1);
 	}
